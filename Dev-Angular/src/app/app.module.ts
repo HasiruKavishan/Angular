10836bdb1ad1702e17ee6f443fa73ac8382c7b7a
HasiruKavishan <bgh.kavishan@gmail.com>
2017-08-18 13:53:13 +0530

    Doesnot working sor far

--- Dev-Angular/src/app/app.module.ts
+++ Dev-Angular/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import {ServerComponent} from "./server/server.component"
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import {ServerComponent} from "./server/server.component"
     ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
