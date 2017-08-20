a89156853a830448d7e4aee33e41cc9aaa1aed06
HasiruKavishan <bgh.kavishan@gmail.com>
2017-08-20 13:07:15 +0530

    Set Some css

--- Dev-Angular/src/app/servers/servers.component.ts
+++ Dev-Angular/src/app/servers/servers.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
+
 })
 export class ServersComponent implements OnInit {
 
